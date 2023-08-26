c66c314e1fc7890107e9b6197bb1439509898547
David Holbrook <david@davidholbrook>
2023-08-26 11:44:43 -0500

    inital testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [games] = useState({
+    game: [
+      {id:1, title:"farcry6"},
+      {id:2, title:"arkham knight"},
+      {id:3, title:"pokemon red"},
+      {id:4, title:"pokemon scarlet"}
+    ]
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <h1 className="text-2xl text-orange-200">Random Game Chooser</h1>
+        <p className='text-white'>{games.game[3].title}</p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;

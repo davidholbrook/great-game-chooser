ffbfe4297c301fb9b30f4edcb3c767cd733d2f0d
David Holbrook <david@davidholbrook>
2024-03-11 13:12:00 -0500

    More contrast tweaking

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
    <div className="container mx-auto">
     <h1 className="text-[#fffdfb] text-center font-bold text-3xl pt-14 mb-8">Great Game Chooser</h1>
       <div className="gamelist min-h-[60vh]">
-        <h2 className="text-orange-600 font-bold drop-shadow-sm text-3xl pt-5 mb-8 text-center">Your Game List</h2>
+        <h2 className="text-black font-bold drop-shadow-sm text-3xl pt-5 mb-8 text-center">Your Game List</h2>
         <div className="w-2/3 mx-auto mt-5">
           {Object.keys(items).map((key, i) => (
             <div className="bg-white border-black border mb-2 p-2 relative" key={key}>
@@ -38,7 +38,7 @@ export default function Home() {
             </div>
           ))}
           <div className="flex justify-center mt-10">
-            <button type="button" className="bg-orange-600 py-3 px-5 uppercase font-bold cursor">Choose next game</button>
+            <button type="button" className="bg-gray-300 py-3 px-5 uppercase font-bold cursor">Choose next game</button>
           </div>
         </div>
       </div>

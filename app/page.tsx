a2b98a1415181bdecb075616cd39edb9382b8f91
David Holbrook <david@davidholbrook>
2024-03-11 13:09:12 -0500

    Tweaked text colors for contrast purposes

--- app/page.tsx
+++ app/page.tsx
@@ -28,15 +28,18 @@ export default function Home() {
 
   return (
    <div className="container mx-auto">
-    <h1 className="text-[#C6C6C6] text-center text-3xl pt-14 mb-8">Great Game Chooser</h1>
+    <h1 className="text-[#fffdfb] text-center font-bold text-3xl pt-14 mb-8">Great Game Chooser</h1>
       <div className="gamelist min-h-[60vh]">
-        <h2 className="text-[#FF8535] font-semibold text-2xl pt-3 text-center">Your Game List</h2>
+        <h2 className="text-orange-600 font-bold drop-shadow-sm text-3xl pt-5 mb-8 text-center">Your Game List</h2>
         <div className="w-2/3 mx-auto mt-5">
           {Object.keys(items).map((key, i) => (
             <div className="bg-white border-black border mb-2 p-2 relative" key={key}>
               {items[key].game} {platformColoring(items[key].platform)}
             </div>
           ))}
+          <div className="flex justify-center mt-10">
+            <button type="button" className="bg-orange-600 py-3 px-5 uppercase font-bold cursor">Choose next game</button>
+          </div>
         </div>
       </div>
    </div>

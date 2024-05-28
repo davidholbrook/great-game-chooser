d2cd127658194ae57ff6350dbeffdece5b4726b1
David Holbrook <david@davidholbrook>
2024-05-28 16:46:23 -0500

    Added useContext and delete function

---
+++ app/_context/context.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { createContext, useContext, useState, useEffect } from 'react'
+
+const GameContext = createContext(undefined);
+
+export function GameProvider({children}) {
+    const [items, setItems] = useState({})
+
+    // The syncing magic
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('games') || '""') ;
+        if (items) setItems(items);
+    }, []);
+
+    useEffect(() => {
+        if(items.length > 0){
+            localStorage.setItem('games', JSON.stringify(items));
+        }
+	}, [items]);
+
+    return (
+        <GameContext.Provider value={{items, setItems,}}>
+            {children}
+        </GameContext.Provider>
+    )
+}
+
+export function useGame() {
+    const context = useContext(GameContext)
+
+    if(!context)
+            throw new Error ('useGame must be used in a `GameProvider`')
+    
+    return context
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
 "use client"
 
-import react, {useState, useEffect} from 'react';
-import Image from 'next/image';
+import react from 'react';
+
 import Icons from './utils/icons';
+import {useGame} from './_context/context'
 
 export default function Home() {
+const {items, setItems} = useGame();
 
-  const [items, setItems]:any = useState([])
-
-  // The syncing magic
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('games') || '""') ;
-    if (items) setItems(items);
-  }, []);
+  function deleteItem(id:number) {
+    let newItems = items.filter((item:any) => item.id !== id);
+    setItems([...newItems]);
+    console.log(id)
+}
 
 // Game Library Page
 return(
-  <div className="container mx-auto flex flex-col justify-center items-center min-h-screen">
-    <Image src="/img/logo.svg" alt="Great Game Chooser logo" width={460} height={50} className="mb-5" />
-    <div className="w-10/12 bg-[#CCCCCC] py-5 px-20 dshadows">
+    <>
       <span className="flex justify-center items-baseline pb-5">
         <h2 className="uppercase font-bold text-3xl inline">Your Library</h2><p className="pl-2 inline font-light">What are you currently playing?</p>
       </span>
@@ -27,16 +25,14 @@ return(
           {items[key].game}
           <span className="flex -mr-3">
             <span className={`BG${items[key].platform} flex items-center justify-center -my-3 w-20`}><Icons icon={items[key].platform} /></span>
-            <span className="bg-deleteRed flex items-center -my-3 px-5 border-l border-gray-700"><Icons icon="delete" /></span>
+            <button type="button" className="bg-deleteRed flex items-center -my-3 px-5 border-l border-gray-700" onClick={() => deleteItem(items[key].id)}><Icons icon="delete" /><span className="hidden">Delete</span></button>
           </span>
         </div>
       ))}
-    <div className="flex justify-between mt-6">
-      <button type="button" className="border-4 border-gray-900 font-bold rounded-md px-3 py-1">Add New Game</button>
-      <button type="button" className="border-4 border-gray-900 bg-gray-900 text-white font-bold rounded-md px-3 py-1">Choose Random Game</button>
+      <div className="flex justify-between mt-6">
+        <button type="button" className="border-4 border-gray-900 font-bold rounded-md px-3 py-1">Add New Game</button>
+        <button type="button" className="border-4 border-gray-900 bg-gray-900 text-white font-bold rounded-md px-3 py-1">Choose Random Game</button>
     </div>
-    </div>
-    <div className="credits">Credits</div>
-  </div>
+  </>
   )
 }
